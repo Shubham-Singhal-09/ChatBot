cd8f208c591d08011f494e24062aa4d38fcdeb36
Shubham Singhal <shubham.singhal@devot.ai>
2023-08-03 15:49:35 +0530

    first commit

---
+++ absa.py
@@ -0,0 +1,90 @@
+import os 
+import logging
+import sys
+
+# Setup OPENAI_API_KEY
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+# Setup logging
+
+log = logging.getLogger(__name__)
+logging.basicConfig(format="%(asctime)s | %(levelname)s | %(message)s", level=logging.INFO)
+
+# Update sys.path (or use PYTHONPATH)
+
+sys.path.insert(0, '..')
+
+
+def read_feedback():
+    import pandas as pd
+
+    df = pd.read_csv("./feedbacks.csv").dropna()
+
+    df.head()
+    
+def filter_and_sampling():
+    # Filter feedbacks text below 300 characters
+    df = df[df.text.str.len() < 300].reset_index(drop=True)
+
+    # Sample n feedbacks
+    df = df.sample(n=15).reset_index()
+
+    df.head()
+    
+def analysis(text):
+    from random import choice
+    from tqdm.notebook import tqdm
+    from gpt3 import analyze
+    from json import loads
+    from pprint import pprint
+    from textwrap import dedent
+
+    analysis_results = []
+    extra_prompts = []
+
+    logging.getLogger("openai").setLevel(logging.INFO)
+    logging.getLogger("requests").setLevel(logging.WARNING)
+
+    # for i in tqdm(range(len(text)), desc="Analyzing reviews"):
+        # title = df.loc[i, "title"]
+        # text = df.loc[i, "text"]
+
+        # log.info(f"Analyzing feedback - \nTitle: {title}\nText: {text}\n")
+
+    extra_prompt = choice(extra_prompts) if extra_prompts else ""
+
+    res = analyze(
+        text=text,
+        extra_prompt="",
+        max_tokens=1024,
+        temperature=0.1,
+        top_p=1,
+    )
+    
+    
+
+    raw_json = res["choices"][0]["text"].strip()
+
+    try:
+        json_data = loads(raw_json)
+        analysis_results.append(json_data)
+
+        log.debug(f"JSON response: {pprint(json_data)}")
+
+        extra_prompts.append(f"\n{text}\n{raw_json}")
+        return raw_json
+       
+    except Exception as e:
+        log.error(f"Failed to parse '{raw_json}' -> {e}")
+        analysis_results.append([])
+        
+
+    # df["analysis"] = analysis_results
+    # df.to_csv("./feedbacks_analysis.csv", index=False)
+    
+    
+
+    
+        
+
---
+++ chatbot.py
@@ -0,0 +1,31 @@
+# Importing required packages
+import streamlit as st
+import openai
+from absa import analysis
+
+st.title("Chatting with ChatGPT")
+st.sidebar.header("Instructions")
+st.sidebar.info(
+    '''This is a web application that allows to sentimenet analyse feedback you enter with 
+       the OpenAI API's implementation of the ChatGPT model.
+       Enter a **feedback** in the **text box** and **press enter** to receive 
+       **sentiment analysis** from the ChatGPT
+       '''
+    )
+
+# Set the model engine and your OpenAI API key
+model_engine = "text-davinci-003"
+openai.api_key = "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" #follow step 4 to get a secret_key
+
+def main():
+    '''
+    This function gets the user input, pass it to ChatGPT function and 
+    displays the response
+    '''
+    # Get user input
+    user_query = st.text_input("Enter feedack here, to exit enter :q", "This product is good but the battery doesn't last. It's lightweight and very easy to use. Well worth the money")
+    analysis_results=analysis(user_query)
+
+    return st.write(f"{user_query} {analysis_results}")
+# call the main function
+main() 
